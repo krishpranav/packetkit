cfd3d064b92d4fc16074add5fb10b091aa2c1997
krishpranav <krisna.pranav@gmail.com>
2021-08-28 19:27:19 +0530

    examples: boxxy rs

---
+++ examples/boxxy.rs
@@ -0,0 +1,30 @@
+#[macro_use] extern crate boxxy;
+extern crate packetkit;
+extern crate env_logger;
+
+fn stage1(sh: &mut boxxy::Shell, _args: Vec<String>) -> Result<(), boxxy::Error> {
+    shprintln!(sh, "[*] starting stage1");
+    packetkit::sandbox::activate_stage1(false).unwrap();
+    shprintln!(sh, "[+] activated!");
+    Ok(())
+}
+
+fn stage2(sh: &mut boxxy::Shell, _args: Vec<String>) -> Result<(), boxxy::Error> {
+    shprintln!(sh, "[*] starting stage2");
+    packetkit::sandbox::activate_stage2(false).unwrap();
+    shprintln!(sh, "[+] activated!");
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+
+    println!("stage1        activate sandbox stage1/2");
+    println!("stage2        activate sandbox stage2/2");
+
+    let toolbox = boxxy::Toolbox::new().with(vec![
+            ("stage1", stage1),
+            ("stage2", stage2),
+        ]);
+    boxxy::Shell::new(toolbox).run()
+}
