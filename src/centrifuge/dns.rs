834b4eae18f9cd3123e42a081b001c99b91ad97c
krishpranav <krisna.pranav@gmail.com>
2021-08-29 14:34:01 +0530

    src: centrifuge dns

---
+++ src/centrifuge/dns.rs
@@ -0,0 +1,23 @@
+use crate::structs::{self, CentrifugeError};
+
+pub fn extract(remaining: &[u8]) -> Result<structs::dns::DNS, CentrifugeError> {
+    if let Ok(dns) = dns_parser::Packet::parse(remaining) {
+        if dns.header.query {
+            let questions = dns.questions
+                               .into_iter()
+                               .map(|q| (q.qtype.into(), q.qname.to_string()))
+                               .collect();
+
+            Ok(structs::dns::Request::new(questions).wrap())
+        } else {
+            let answers = dns.answers
+                             .into_iter()
+                             .map(|a| (a.name.to_string(), structs::dns::Record::from(a.data)))
+                             .collect();
+
+            Ok(structs::dns::Response::new(answers).wrap())
+        }
+    } else {
+        Err(structs::CentrifugeError::WrongProtocol)
+    }
+}
