d4c0163620c074307413a6fc153204da236daf3e
krishpranav <krisna.pranav@gmail.com>
2021-08-29 14:36:23 +0530

    src: centrifuge dropbox

---
+++ src/centrifuge/dropbox.rs
@@ -0,0 +1,12 @@
+use std::str;
+
+use crate::structs::{dropbox, CentrifugeError};
+
+
+pub fn extract(data: &[u8]) -> Result<dropbox::DropboxBeacon, CentrifugeError> {
+    let data = str::from_utf8(data)
+                    .map_err(|_| CentrifugeError::InvalidPacket)?;
+    let beacon = serde_json::from_str(data)
+                    .map_err(|_| CentrifugeError::InvalidPacket)?;
+    Ok(beacon)
+}
